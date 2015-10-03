6ee90097819e48dffe2dd0ee04af30f89d266c17
leila <chenyuyun.leila@gmail.com>
2015-10-03 13:48:32 +0800

    complete fibonacci sequence - iterative version

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,26 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
-
-	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+	push {r3, r4, r5, r6, lr}
+	@ R0 = x
+	@ R3 = before
+	mov r3, #-1 
+	@ R4 = current
+	mov r4, #1
+	@ R5 = i
+	mov r5, r0
+	@ R6 = sum
+	mov r6, #0
+.for:
+	add r6, r4, r3
+	mov r3, r4
+	mov r4, r6
+	subs r5, r5 ,#1
+	bge .for
+	@ return sum
+	mov r0, r6	
+	pop {r3, r4, r5, r6, pc}
 
 	.size fibonacci, .-fibonacci
 	.end
+
